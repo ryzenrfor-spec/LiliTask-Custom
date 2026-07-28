8b7ae3486c7e6b952b8804ead019cfcffd299152
Putra Erlangga <ryzenrfor@gmail.com>
2026-07-28 23:13:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const ASSETS_TO_CACHE = [
   './style.css',
   './script.js',
   './manifest.json',
-  './icon.jpg'
+  './icon.jpeg'
 ];
 
 // Install event: langsung skip waiting agar service worker baru langsung aktif
